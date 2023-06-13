1c49fdd4fa921695363e41211a823fff8c32a4f9
shytuleva24 <shutyleva@gmail.com>
2023-06-13 23:08:12 +0300

    optimization code sea component add animation tehnology

--- src/app/sky/sky.component.ts
+++ src/app/sky/sky.component.ts
@@ -1,134 +1,161 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 
 @Component({
-  selector: 'app-sky',
-  templateUrl: './sky.component.html',
-  styleUrls: ['./sky.component.css']
+    selector: 'app-sky',
+    templateUrl: './sky.component.html',
+    styleUrls: ['./sky.component.css']
 })
 export class SkyComponent implements AfterViewInit {
-  @ViewChild('c', {static: true}) canvasRef!: ElementRef<HTMLCanvasElement>;
+    @ViewChild('c', {static: true}) canvasRef!: ElementRef<HTMLCanvasElement>;
 
-  scene!: THREE.Scene;
-  camera!: THREE.PerspectiveCamera;
-  renderer!: THREE.WebGLRenderer;
-  car!: THREE.Object3D;
-  mouseX = 0;
-  mouseY = 0;
-  windowHalfX = window.innerWidth / 2;
-  windowHalfY = window.innerHeight / 2;
-  ship: THREE.Object3D | undefined;
-  ngAfterViewInit() {
-    this.init();
-  }
+    scene!: THREE.Scene;
+    camera!: THREE.PerspectiveCamera;
+    renderer!: THREE.WebGLRenderer;
+    car!: THREE.Object3D;
+    mouseX = 0;
+    mouseY = 0;
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+    ship: THREE.Object3D | undefined;
+    intersectionObserver!: IntersectionObserver;
+    isIntersecting = false;
 
-  init() {
-    const canvas = this.canvasRef.nativeElement;
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xdddddd);
-
-    this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 3000);
-    this.renderer = new THREE.WebGLRenderer({canvas, antialias: true});
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    const shipObject = this.scene.getObjectByName("ship");
-    console.log(shipObject);
-    if (shipObject) {
-      this.camera.lookAt(shipObject.position);
-
-      const cameraOffset = new THREE.Vector3(0, 5, -15);
-      this.camera.position.copy(shipObject.position).add(cameraOffset);
+    ngAfterViewInit() {
+        this.init();
+        this.initIntersectionObserver();
     }
-    const loader = new GLTFLoader();
-    loader.load('assets/scatch/scene.gltf', (gltf) => {
-      this.car = gltf.scene.children[0];
-      this.car.scale.set(1, 1, 1);
-      this.scene.add(gltf.scene);
-      this.scene.traverse((object: THREE.Object3D) => {
-        if (object.name === "Boot_Finaal_1") { // Замените "Boot_Finaal_1" на имя вашего корабля
-          this.ship = object;
-          const cameraOffset = new THREE.Vector3(0, 5, -10);
-          this.camera.position.copy(object.position).add(cameraOffset);
+
+    initIntersectionObserver() {
+        const options = {
+            root: null, // Область видимости по умолчанию - вся страница
+            threshold: 0 // Порог видимости элемента (0 означает, что элемент видим, когда его границы пересекают границы видимой области)
+        };
+
+        this.intersectionObserver = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    this.isIntersecting = true;
+                    this.animate(); // Возобновляем анимацию при появлении блока в видимой области
+                } else {
+                    this.isIntersecting = false;
+                }
+            });
+        }, options);
+
+        this.intersectionObserver.observe(this.canvasRef.nativeElement);
+    }
+
+
+    init() {
+        const canvas = this.canvasRef.nativeElement;
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0xdddddd);
+
+        this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 3000);
+        this.renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        const shipObject = this.scene.getObjectByName("ship");
+        console.log(shipObject);
+        if (shipObject) {
+            this.camera.lookAt(shipObject.position);
+
+            const cameraOffset = new THREE.Vector3(0, 5, -15);
+            this.camera.position.copy(shipObject.position).add(cameraOffset);
         }
-      });
-      this.animate();
-    });
+        const loader = new GLTFLoader();
+        loader.load('assets/scatch/scene.gltf', (gltf) => {
+            this.car = gltf.scene.children[0];
+            this.car.scale.set(1, 1, 1);
+            this.scene.add(gltf.scene);
+            this.scene.traverse((object: THREE.Object3D) => {
+                if (object.name === "Boot_Finaal_1") { // Замените "Boot_Finaal_1" на имя вашего корабля
+                    this.ship = object;
+                    const cameraOffset = new THREE.Vector3(0, 5, -10);
+                    this.camera.position.copy(object.position).add(cameraOffset);
+                }
+            });
+            this.animate();
+        });
 
-    window.addEventListener('resize', () => this.onWindowResize());
-    document.addEventListener('mousemove', (event: MouseEvent) => this.onDocumentMouseMove(event));
-    document.addEventListener('touchstart', (event: TouchEvent) => this.onDocumentTouchStart(event));
-    document.addEventListener('touchmove', (event: TouchEvent) => this.onDocumentTouchMove(event));
-  }
-
-  onWindowResize() {
-    this.windowHalfX = window.innerWidth / 2;
-    this.windowHalfY = window.innerHeight / 2;
-
-    this.camera.aspect = window.innerWidth / window.innerHeight;
-    this.camera.updateProjectionMatrix();
-
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-  }
-
-  onDocumentMouseMove(event: MouseEvent) {
-    this.mouseX = (event.clientX - this.windowHalfX) / 250;
-    this.mouseY = (event.clientY - this.windowHalfY) / 250;
-  }
-
-  onDocumentTouchStart(event: TouchEvent) {
-    if (event.touches.length === 1) {
-      event.preventDefault();
-      // this.mouseX = event.touches[0].pageX - this.windowHalfX;
-      // this.mouseY = event.touches[0].pageY - this.windowHalfY;
-    }
-  }
-
-  onDocumentTouchMove(event: TouchEvent) {
-    if (event.touches.length === 1) {
-      event.preventDefault();
-      // this.mouseX = event.touches[0].pageX - this.windowHalfX;
-      // this.mouseY = event.touches[0].pageY - this.windowHalfY;
-    }
-  }
-
-  animate() {
-    if (this.ship) {
-      const shipPosition = this.ship.position;
-      const cameraRadius = 500; // Расстояние от камеры до корабля
-      const cameraRotationSpeed = 0.00005; // Скорость вращения камеры
-      const cameraOffset = new THREE.Vector3(0, 5, 0); // Смещение камеры относительно корабля
-
-      const targetX = shipPosition.x + cameraRadius * Math.sin(cameraRotationSpeed + this.mouseX);
-      const targetZ = shipPosition.z + cameraRadius * Math.cos(cameraRotationSpeed + this.mouseX);
-      const targetY = shipPosition.y + cameraOffset.y + this.mouseY;
-
-      const rotationSpeed = 0.001; // Скорость вращения камеры
-      const smoothness = 0.1; // Сглаживание движения камеры
-
-      this.camera.position.x += (targetX - this.camera.position.x) * smoothness;
-      this.camera.position.z += (targetZ - this.camera.position.z) * smoothness;
-      this.camera.position.y += (targetY - this.camera.position.y) * smoothness;
-
-      const targetRotationY = -this.mouseX;
-      const targetRotationX = -this.mouseY;
-
-      const rotationThreshold = 0.0001; // Порог для остановки вращения
-
-      if (Math.abs(this.camera.rotation.y - targetRotationY) > rotationThreshold ||
-          Math.abs(this.camera.rotation.x - targetRotationX) > rotationThreshold) {
-        this.camera.rotation.y += (targetRotationY - this.camera.rotation.y) * rotationSpeed;
-        this.camera.rotation.x += (targetRotationX - this.camera.rotation.x) * rotationSpeed;
-      } else {
-        this.camera.rotation.y += rotationSpeed;
-      }
-
-      this.camera.lookAt(shipPosition);
+        window.addEventListener('resize', () => this.onWindowResize());
+        document.addEventListener('mousemove', (event: MouseEvent) => this.onDocumentMouseMove(event));
+        document.addEventListener('touchstart', (event: TouchEvent) => this.onDocumentTouchStart(event));
+        document.addEventListener('touchmove', (event: TouchEvent) => this.onDocumentTouchMove(event));
     }
 
-    this.renderer.render(this.scene, this.camera);
-    requestAnimationFrame(() => this.animate());
-  }
+    onWindowResize() {
+        this.windowHalfX = window.innerWidth / 2;
+        this.windowHalfY = window.innerHeight / 2;
 
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    onDocumentMouseMove(event: MouseEvent) {
+        this.mouseX = (event.clientX - this.windowHalfX) / 250;
+        this.mouseY = (event.clientY - this.windowHalfY) / 250;
+    }
+
+    onDocumentTouchStart(event: TouchEvent) {
+        if (event.touches.length === 1) {
+            event.preventDefault();
+            // this.mouseX = event.touches[0].pageX - this.windowHalfX;
+            // this.mouseY = event.touches[0].pageY - this.windowHalfY;
+        }
+    }
+
+    onDocumentTouchMove(event: TouchEvent) {
+        if (event.touches.length === 1) {
+            event.preventDefault();
+            // this.mouseX = event.touches[0].pageX - this.windowHalfX;
+            // this.mouseY = event.touches[0].pageY - this.windowHalfY;
+        }
+    }
+
+    animate() {
+        if (!this.isIntersecting) {
+            return; // Если блок не видим, прерываем анимацию
+        }
+        if (this.ship) {
+            const shipPosition = this.ship.position;
+            const cameraRadius = 500; // Расстояние от камеры до корабля
+            const cameraRotationSpeed = 0.00005; // Скорость вращения камеры
+            const cameraOffset = new THREE.Vector3(0, 5, 0); // Смещение камеры относительно корабля
+
+            const targetX = shipPosition.x + cameraRadius * Math.sin(cameraRotationSpeed + this.mouseX);
+            const targetZ = shipPosition.z + cameraRadius * Math.cos(cameraRotationSpeed + this.mouseX);
+            const targetY = shipPosition.y + cameraOffset.y + this.mouseY;
+
+            const rotationSpeed = 0.001; // Скорость вращения камеры
+            const smoothness = 0.1; // Сглаживание движения камеры
+
+            this.camera.position.x += (targetX - this.camera.position.x) * smoothness;
+            this.camera.position.z += (targetZ - this.camera.position.z) * smoothness;
+            this.camera.position.y += (targetY - this.camera.position.y) * smoothness;
+
+            const targetRotationY = -this.mouseX;
+            const targetRotationX = -this.mouseY;
+
+            const rotationThreshold = 0.0001; // Порог для остановки вращения
+
+            if (Math.abs(this.camera.rotation.y - targetRotationY) > rotationThreshold ||
+                Math.abs(this.camera.rotation.x - targetRotationX) > rotationThreshold) {
+                this.camera.rotation.y += (targetRotationY - this.camera.rotation.y) * rotationSpeed;
+                this.camera.rotation.x += (targetRotationX - this.camera.rotation.x) * rotationSpeed;
+            } else {
+                this.camera.rotation.y += rotationSpeed;
+            }
+
+            this.camera.lookAt(shipPosition);
+        }
+
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(() => this.animate());
+    }
 
 
 }
--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 interface Block {
   title: string;
@@ -12,6 +12,9 @@ interface Block {
   styleUrls: ['./technology.component.css']
 })
 export class TechnologyComponent implements OnInit {
+  @ViewChild('maskContainer') maskContainerRef!: ElementRef;
+  @ViewChild('blockElement') blockElement!: ElementRef;
+
   blocks: Block[] = [
     {
       title: 'Markup',
@@ -36,6 +39,9 @@ export class TechnologyComponent implements OnInit {
   ];
 
   currentBlockIndex = 0;
+  maskStartHeight: string| number = '0px';
+  maskActiveHeight: string| number  = '0px';
+  maskEndHeight: string| number  = '100%';
 
   ngOnInit() {
     this.activateBlock(this.currentBlockIndex);
@@ -54,6 +60,7 @@ export class TechnologyComponent implements OnInit {
 
         if (isBlockVisible && !block.isActive) {
           this.activateBlock(index);
+          this.calculateMaskHeight(index);
         }
       }
     });
@@ -63,5 +70,39 @@ export class TechnologyComponent implements OnInit {
     this.blocks.forEach((block, i) => {
       block.isActive = i === index;
     });
+  }
+
+  calculateMaskHeight(index: number) {
+    const blockElement = document.querySelector(`#block-${index}`);
+    // if (blockElement){
+    //   console.log(blockElement.offsetTop);
+    // }
+    const maskContainerHeight = this.maskContainerRef.nativeElement.clientHeight;
+    const activeBlockHeight = this.blockElement.nativeElement.clientHeight;
+    const maskStartHeight = activeBlockHeight * index + (40 * index);
+    const maskEndHeight = maskContainerHeight - activeBlockHeight - maskStartHeight;
+
+    this.maskStartHeight = maskStartHeight + 'px';
+    this.maskActiveHeight = activeBlockHeight + 40 + 'px';
+    this.maskEndHeight = maskEndHeight + 40 + 'px';
+
+  //   const activeBlockElement = document.querySelector(`#block-${index}`);
+  //   if (activeBlockElement) {
+  //     const skillsElement = activeBlockElement.querySelector('.skill-items');
+  //     const maskStartElement = document.querySelector('.mask-start');
+  //     const maskActiveElement = document.querySelector('.mask-active');
+  //     const maskEndElement = document.querySelector('.mask-end');
+  //
+  //     if (skillsElement && maskStartElement && maskActiveElement && maskEndElement) {
+  //       const skillsHeight = skillsElement.clientHeight;
+  //       const maskActiveHeight = activeBlockElement.clientHeight;
+  //       const maskStartHeight = `${maskActiveHeight}px`;
+  //       const maskEndHeight = `${skillsHeight - maskActiveHeight}px`;
+  //
+  //       this.maskStartHeight = maskStartHeight;
+  //       this.maskActiveHeight = maskActiveHeight;
+  //       this.maskEndHeight = maskEndHeight;
+  //     }
+  //   }
   }
 }
